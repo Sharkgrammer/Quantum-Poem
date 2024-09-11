fda697105bac382e1fda65031010542f5d53e3e2
Daniel Keane Kelly <danielkeanekelly@gmail.com>
2024-09-11 18:02:05 +0100

    Added Stars

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,6 @@
 import {useEffect, useState} from 'react'
+import Particles, {initParticlesEngine} from "@tsparticles/react";
+import {loadSlim} from "@tsparticles/slim";
 
 import tree from './assets/tree.png'
 import line1 from './assets/line1.png'
@@ -8,27 +10,115 @@ import line4 from './assets/line4.png'
 import music from './assets/timberhearth.mp3'
 
 function App() {
+    const [init, setInit] = useState(false);
     const [isPlaying, setIsPlaying] = useState(false);
 
     function audio() {
-
-        console.log(isPlaying);
-
+        // Setting state to ensure that the audio is only played once
         if (!isPlaying) {
             let audio = new Audio(music);
 
             setIsPlaying(true);
+
+            audio.volume = 0.4;
             audio.play().then(r => setIsPlaying(false));
         }
-
     }
 
-    useEffect(audio, []);
+    useEffect(() => {
+        audio();
 
+        initParticlesEngine(async (engine) => {
+            // you can initiate the tsParticles instance (engine) here, adding custom shapes or presets
+            // this loads the tsparticles package bundle, it's the easiest method for getting everything ready
+            // starting from v2 you can add only the features you need reducing the bundle size
+            //await loadAll(engine);
+            //await loadFull(engine);
+            await loadSlim(engine);
+            //await loadBasic(engine);
+        }).then(() => {
+            setInit(true);
+        });
+    }, []);
 
+    const particlesLoaded = (container) => {
+        console.log(container);
+    };
     return (
-        <div className="w-full full bg-black flex justify-center px-5">
-            <QPoem/>
+        <div className="w-full full flex justify-center px-5 select-none bg-gray-950">
+            <div className="z-0">
+                {init && <Particles
+                    id="tsparticles"
+                    particlesLoaded={particlesLoaded}
+                    options={{
+                        "particles": {
+                            "number": {
+                                "value": 160,
+                                "density": {
+                                    "enable": true,
+                                    "value_area": 800
+                                }
+                            },
+                            "color": {
+                                "value": "#ffffff"
+                            },
+                            "shape": {
+                                "type": "circle",
+                                "stroke": {
+                                    "width": 0,
+                                    "color": "#000000"
+                                },
+                                "polygon": {
+                                    "nb_sides": 5
+                                },
+                                "image": {
+                                    "src": "img/github.svg",
+                                    "width": 100,
+                                    "height": 100
+                                }
+                            },
+                            "opacity": {
+                                "value": 1,
+                                "random": true,
+                                "anim": {
+                                    "enable": true,
+                                    "speed": 1,
+                                    "opacity_min": 0,
+                                    "sync": false
+                                }
+                            },
+                            "size": {
+                                "value": 3,
+                                "random": true,
+                                "anim": {
+                                    "enable": false,
+                                    "speed": 4,
+                                    "size_min": 0.3,
+                                    "sync": false
+                                }
+                            },
+                            "move": {
+                                "enable": true,
+                                "speed": 1,
+                                "direction": "none",
+                                "random": true,
+                                "straight": false,
+                                "out_mode": "out",
+                                "bounce": false,
+                                "attract": {
+                                    "enable": false,
+                                    "rotateX": 600,
+                                    "rotateY": 600
+                                }
+                            }
+                        }
+                    }}
+                />
+                }
+            </div>
+
+            <QPoem />
+
         </div>
     )
 }
@@ -56,7 +146,7 @@ function QPoem() {
 
 
     return (
-        <div className="w-full grid [grid-template-areas:'stack']" onClick={setPoems}>
+        <div className="w-full grid [grid-template-areas:'stack'] z-50" onClick={setPoems}>
 
             <div className="[grid-area:stack] w-full">
                 <div className="flex justify-center items-center overflow-hidden">
@@ -74,7 +164,6 @@ function QPoem() {
                 </div>
 
             </div>
-
 
         </div>
     )
